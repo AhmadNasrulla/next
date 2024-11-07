8b56ba6ea666ead9339b8b77e0005cd155f0720a
AhmadNasrulla <ahmadnasrullahbashir@gmail.com>
2024-11-07 06:54:58 +0500

    firebase

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -10,13 +10,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCNQUZip2SDLuEaeRi6NchydQotNTuAw8o",
-  authDomain: "website-18c7d.firebaseapp.com",
-  projectId: "website-18c7d",
-  storageBucket: "website-18c7d.appspot.com",
-  messagingSenderId: "518439308844",
-  appId: "1:518439308844:web:4c8944aff0d6c7be3f8b89",
-  measurementId: "G-Q6GLRN11E3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
